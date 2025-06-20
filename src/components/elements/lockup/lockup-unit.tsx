8e072e229676d583191260bbe72cdede97b1e7e6
Jen Breese-Kauth <jbreese@stanford.edu>
2025-06-19 18:18:42 -0700

    fix to logo size and gap between line

--- src/components/elements/lockup/lockup-unit.tsx
+++ src/components/elements/lockup/lockup-unit.tsx
@@ -1,17 +1,16 @@
 import LockupLogo from "@components/elements/lockup/lockup-logo"
 import {LockupProps} from "@components/elements/lockup/lockup"
 
-// No equivalent in site's lockup
 const LockupUnit = ({line1, siteName, logoUrl}: LockupProps) => {
   return (
     <div className="py-10">
-      <div className="flex gap-4">
-        <div className="mb-4 flex gap-4">
+      <div className="flex gap-[3px]">
+        <div className="mb-4 flex gap-[4.5px]">
           <LockupLogo logoUrl={logoUrl} siteName={siteName} />
           <div className="w-[1px] shrink-0 bg-black" />
         </div>
 
-        <div className="type-4 self-end font-normal text-black">{line1 || siteName}</div>
+        <div className="type-4 relative top-[3px] self-end font-normal text-black">{line1 || siteName}</div>
       </div>
     </div>
   )
