d98076e5372b3243ee1b7844a610e458d6625657
Jen Breese-Kauth <jbreese@stanford.edu>
2025-06-25 14:01:39 -0700

    fixup

--- src/components/elements/lockup/lockup-unit.tsx
+++ src/components/elements/lockup/lockup-unit.tsx
@@ -4,13 +4,13 @@ import {LockupProps} from "@components/elements/lockup/lockup"
 const LockupUnit = ({line1, siteName, logoUrl}: LockupProps) => {
   return (
     <div className="py-10">
-      <div className="flex gap-[3px]">
-        <div className="mb-4 flex gap-[4.5px]">
+      <div className="flex gap-[27px]">
+        <div className="mb-4 flex gap-[27px]">
           <LockupLogo logoUrl={logoUrl} siteName={siteName} />
-          <div className="w-[1px] shrink-0 bg-black" />
+          <div className="w-[6px] shrink-0 bg-black" />
         </div>
 
-        <div className="type-4 relative top-[3px] self-end font-normal text-black">{line1 || siteName}</div>
+        <div className="relative top-[8px] self-end text-[102px] font-normal text-black">{line1 || siteName}</div>
       </div>
     </div>
   )
