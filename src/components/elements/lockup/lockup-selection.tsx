94969181bc10b82b9bf1a07bd48ba8c353405d70
Jen Breese <jbreese@stanford.edu>
2026-01-30 11:47:32 -0800

    Changed alignment and size of remaining character count (#9)

--- src/components/elements/lockup/lockup-selection.tsx
+++ src/components/elements/lockup/lockup-selection.tsx
@@ -311,16 +311,18 @@ const LockupInput = ({
     setCount(e.target?.value.length || 0)
   }
   return (
-    <div className={clsx("mb-10 flex items-center gap-5", {hidden})}>
-      <label htmlFor={id}>{label}</label>
-      <input
-        className="p-25 h-[40px] w-[250px] text-3xl"
-        id={id}
-        onChange={onInputChange}
-        defaultValue={defaultValue}
-        maxLength={45}
-      />
-      <span className="text-cardinal-red">Remaining: {45 - count}</span>
+    <div className={clsx("mb-10 flex w-fit flex-col", {hidden})}>
+      <div className="flex items-center gap-5">
+        <label htmlFor={id}>{label}</label>
+        <input
+          className="p-25 h-[40px] w-[250px] text-3xl"
+          id={id}
+          onChange={onInputChange}
+          defaultValue={defaultValue}
+          maxLength={45}
+        />
+      </div>
+      <div className="text-right text-[1.5rem] text-cardinal-red">Remaining: {45 - count}</div>
     </div>
   )
 }
